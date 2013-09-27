6d48e0334dd61846bbf72e393af60ed3e551b77c
Rychetsky <rychetsky@me.com>
2013-09-27 11:09:54 +0200

    Menü und Inhalt mit PHP generieren lassen

--- index.php
+++ index.php
@@ -5,12 +5,22 @@ spl_autoload_register(function($class){
 	require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
 });
 
-# Get Markdown class
-use \Michelf\Markdown;
+# Get MarkdownExtra class
+use \Michelf\MarkdownExtra;
 
-# Read file and pass content through the Markdown praser
-$text = file_get_contents('txt/file.md');
-$html = Markdown::defaultTransform($text);
+
+# Array mit allen Seiten
+
+$content = array(	'einleitung' => 'MIDI', 
+					'einfluss' => 'Einfluss', 
+					'timeline' => 'Timeline', 
+					'technik' => 'Technik', 
+					'unternehmen' => 'Unternehmen & Geräte', 
+					'gegenwart' => 'Gegenwart', 
+					'look' => 'Look', 
+					'quellen' => 'Quellen', 
+					'impressum' => 'Impressum'
+				);
 
 ?>
 
@@ -31,284 +41,30 @@ $html = Markdown::defaultTransform($text);
 	
 	<header>
 		
-		
 		<nav class="nav-collapse">
 			<ul>
-				<li><a href="#einleitung" class="brand">MIDI</a></li>
-				<li><a href="#einleitung" class="einleitung">Einleitung</a></li>
-				<li><a href="#einfluss" class="einfluss">Einfluss</a></li>
-				<li><a href="#timeline" class="timeline">Timeline</a></li>
-				<li><a href="#technik" class="technik">Technik</a></li>
-				<li><a href="#unternehmen" class="unternehmen">Unternehmen & Geräte</a></li>
-				<li><a href="#gegenwart" class="gegenwart">Gegenwart</a></li>
-				<li class="divider"></li>
-				<li><a href="#look" class="look">Look</a></li>
-				<li><a href="#quellen" class="quellen">Quellen</a></li>
-				<li><a href="#impressum" class="impressum">Impressum</a></li>
+				<?php # Menü zusammenbauen
+				foreach ($content as $key => $value)
+					echo '<li><a href="#'.$key.'" class="'.$key.'">'.$value.'</a></li>';
+				?>
 			</ul>
 		</nav>
 		
 	</header>
 	
-	
-	<section class="einleitung sides dark" id="einleitung">
-		<div class="inner">
-			
-			<header>
-				<h1>
-					MIDI <span>die frühen Jahre (1980 bis 1989)</span>
-				</h1>
-			</header>
-			
-			<article>
-				<?=Markdown::defaultTransform(file_get_contents('txt/einleitung.md'))?>
-			</article>
-			
-			<aside>
-				
-			</aside>
-			
-			<footer>
-				<h4 class="quellen">Quellen</h4>
-				<p>http://www.midi.org, http://de.wikipedia.org/wiki/Musical_Instrument_Digital_Interface, http://www.midi.org/aboutmidi/tut_history.php</p>
-			</footer>
-			
-		</div>	
-	</section>
-	
-	<div class="mood_einfluss"></div>
+	<?php # Inhalte laden
+	foreach ($content as $key => $value){
+		echo '<div class="mood_'.$key.'"></div>';
+		echo '<section class="'.$key.' sides " id="'.$key.'"><div class="inner">';
+		echo MarkdownExtra::defaultTransform(file_get_contents('txt/'.$key.'.md'));
+		echo '</div></section>';
+	}
+	?>
 
-	<section class="einfluss sides dark" id="einfluss">
-		<div class="inner">
 			
-			<header>
-				<h1>
-					Einfluss <span>Auswirkungen auf die Menschheit</span>
-				</h1>
-			</header>
+		
 			
-			<article class="column2">
-				<?=Markdown::defaultTransform(file_get_contents('txt/einfluss.md'))?>
-			</article>
-			
-			<aside>
-				
-			</aside>
-			
-			<footer>
-				<h4 class="quellen">Quellen</h4>
-				<p>http://www.midi.org, http://de.wikipedia.org/wiki/Musical_Instrument_Digital_Interface, http://www.midi.org/aboutmidi/tut_history.php</p>
-			</footer>
-			
-		</div>	
-	</section>
-
-
-	<div class="mood_timeline"></div>
-
-	<section class="timeline sides dark" id="timeline">
-		<div class="inner">
-			
-			<div class="line"></div>
-			
-			<header>
-				<h1 class="center">
-					Timeline
-				</h1>
-			</header>
-			
-			<article>
-				<dl>
-				  <dt>Ende der 70er Jahre</dt>
-				    <dd>Immer mehr Synthesizer werden Veröffentlicht, es gibt noch keinen einheitlichen Standard, um die Instrumente miteinander zu verbinden.</dd>
-				  <dt>1981</dt>
-				    <dd>Dave Smith, Ikutaru Kakehashi und Tom Oberheim Entwickeln ein Konzept, um die Synthesizer der unterschiedlichen Hersteller miteinander kompatibel zu machen. Erster Name: Universal Synthesizer Interface.</dd>
-				  <dt>1982</dt>
-				    <dd>Das Konzept des Universal Synthesizer Interface wird mit den führenden Herstellern überarbeitet und der Allgemeinheit unter dem Namen MIDI (Musical Instrument Digital Interface) vorgestellt.</dd>
-				  <dt>1982</dt>
-				    <dd>Das Konzept des Universal Synthesizer Interface wird mit den führenden Herstellern überarbeitet und der Allgemeinheit unter dem Namen MIDI (Musical Instrument Digital Interface) vorgestellt.</dd>
-				  <dt>Dezember 1982</dt>
-				    <dd>Das erste Instrument, mit MIDI-Implementierung erscheint: Der Sequential Prophet-600.</dd>
-				  <dt>Januar 1983</dt>
-				    <dd>Der Roland JP6 erscheint als weiterer MIDI-fähiger Synthesizer. Auf der NAMM-Show wird er zusammen mit dem Sequential Prophet-600 verbunden vorgeführt. <br>Der finale MIDI-Standard 1.0 wird veröffentlicht.</dd>
-				  <dt>1984</dt>
-				    <dd>Gründung des MIDI-Verbandes</dd>
-				  <dt>Mitte der 1980er</dt>
-				    <dd>MIDI-Sequencer-Programme für den C64 erscheinen, damit konnten Synthesizer und andere MIDI-Geräte über den Computer gesteuert werden.</dd>
-				  <dt>1986</dt>
-				    <dd>MIDI-Timecode wird veröffentlich</dd>
-				</dl>
-			</article>
-				
-			
-		</div>	
-	</section>
-
-
-	<div class="mood_technik"></div>
-
-	<section class="technik sides dark" id="technik">
-		<div class="inner">
-			
-			<header>
-				<h1>
-					Technik <span>Wie funktioniert MIDI?</span>
-				</h1>
-			</header>
-			
-			<article class="column2">
-				<?=Markdown::defaultTransform(file_get_contents('txt/technik.md'))?>
-			</article>
-			
-			<aside>
-				
-			</aside>
-			
-			<footer>
-				<h4 class="quellen">Quellen</h4>
-				<p>http://www.midi.org, http://de.wikipedia.org/wiki/Musical_Instrument_Digital_Interface, http://www.midi.org/aboutmidi/tut_history.php</p>
-			</footer>
-			
-		</div>	
-	</section>
-
-
-	<div class="mood_unternehmen"></div>
-
-	<section class="unternehmen sides dark" id="unternehmen">
-		<div class="inner">
-			
-			<header>
-				<h1>
-					Unternehmen & Geräte <span></span>
-				</h1>
-			</header>
-			
-			<article class="column2">
-				<?=Markdown::defaultTransform(file_get_contents('txt/unternehmen.md'))?>
-			</article>
-			
-			<aside>
-				
-			</aside>
-			
-			<footer>
-				<h4 class="quellen">Quellen</h4>
-				<p>http://www.midi.org, http://de.wikipedia.org/wiki/Musical_Instrument_Digital_Interface, http://www.midi.org/aboutmidi/tut_history.php</p>
-			</footer>
-			
-		</div>	
-	</section>
-
-
-	<div class="mood_gegenwart"></div>
-
-	<section class="gegenwart sides dark" id="gegenwart">
-		<div class="inner">
-			
-			<header>
-				<h1>
-					Gegenwart <span></span>
-				</h1>
-			</header>
-			
-			<article class="column2">
-				<?=Markdown::defaultTransform(file_get_contents('txt/gegenwart.md'))?>
-			</article>
-			
-			<aside>
-				
-			</aside>
-			
-			<footer>
-				<h4 class="quellen">Quellen</h4>
-				<p>http://www.midi.org, http://de.wikipedia.org/wiki/Musical_Instrument_Digital_Interface, http://www.midi.org/aboutmidi/tut_history.php</p>
-			</footer>
-			
-		</div>	
-	</section>
-
-
-	<div class="mood_look"></div>
-
-	<section class="look sides dark" id="look">
-		<div class="inner">
-			
-			<header>
-				<h1>
-					Look <span></span>
-				</h1>
-			</header>
-			
-			<article class="column2">
-				<?=Markdown::defaultTransform(file_get_contents('txt/look.md'))?>
-			</article>
-			
-			<aside>
-				
-			</aside>
-			
-			<footer>
-				<h4 class="quellen">Quellen</h4>
-				<p>http://www.midi.org, http://de.wikipedia.org/wiki/Musical_Instrument_Digital_Interface, http://www.midi.org/aboutmidi/tut_history.php</p>
-			</footer>
-			
-		</div>	
-	</section>
-
-
-
-	<div class="mood_quellen"></div>
-
-	<section class="quellen sides dark" id="quellen">
-		<div class="inner">
-			
-			<header>
-				<h1>
-					Quellenverzeichnis <span></span>
-				</h1>
-			</header>
-			
-			<article class="column2">
-				<?=Markdown::defaultTransform(file_get_contents('txt/quellen.md'))?>
-			</article>
-			
-			<aside>
-				
-			</aside>
-			
-			<footer>
-			</footer>
-			
-		</div>	
-	</section>
-
-
-
-	<div class="mood_impressum"></div>
-
-	<section class="impressum sides dark" id="impressum">
-		<div class="inner">
-			
-			<header>
-				<h1>
-					Impressum <span></span>
-				</h1>
-			</header>
-			
-			<article class="column2">
-				<?=Markdown::defaultTransform(file_get_contents('txt/impressum.md'))?>
-			</article>
-			
-			<aside>
-				
-			</aside>
-			
-			<footer>
-			</footer>
-			
-		</div>	
-	</section>
+		
 
 
 </div>	
--- txt/einleitung.md
+++ txt/einleitung.md
@@ -1,1 +1,12 @@
-MIDI veränderte Anfang der 1980er Jahre die Musik-Landschaft. Mit dem Musical Instrument Digital Interface wurde ein Austausch von Informationen zwischen Keyboards, Synthesizern und anderen elektronischen Musikinstrumenten möglich. Erstmals gezeigt wurde eine MIDI-Verbindung auf der NAMM-Show 1983 mit dem SEQUENTIAL CIRCUITS PROPHET-600 und dem Roland Jupiter-6. Von da an verbreitete sich der MIDI-Standard immer mehr, und bot Musikern und Musikproduzenten viele neue Möglichkeiten. Es konnten mit einem Keyboard mehrere Klangerzeuger gleichzeitig angesteuert werden, neuartige Instrumente und Controller erschienen, und natürlich wurde der weg frei für digitale Sequenzer.
+<header markdown="1">
+# MIDI <span>die frühen Jahre (1980 bis 1989)</span>
+</header>
+
+<article markdown="1">
+MIDI veränderte Anfang der 1980er Jahre die Musik-Landschaft. Mit dem Musical Instrument Digital Interface wurde ein Austausch von Informationen zwischen Keyboards, Synthesizern und anderen elektronischen Musikinstrumenten möglich. Erstmals gezeigt wurde eine MIDI-Verbindung auf der NAMM-Show 1983 mit dem SEQUENTIAL CIRCUITS PROPHET-600 und dem Roland Jupiter-6. Von da an verbreitete sich der MIDI-Standard immer mehr, und bot Musikern und Musikproduzenten viele neue Möglichkeiten. Es konnten mit einem Keyboard mehrere Klangerzeuger gleichzeitig angesteuert werden, neuartige Instrumente und Controller erschienen, und natürlich wurde der weg frei für digitale Sequenzer.[^midi.org]
+</article>
+<aside markdown="1">
+
+</aside>
+
+[^midi.org]: midi.org, 1986
